303018afce0521159317e7aa44bab872b81fb8f9
qiannnhui <b09901160@ntu.edu.tw>
2025-11-16 12:11:32 +0800

    add:path_248

--- unsupervised_TU/arguments.py
+++ unsupervised_TU/arguments.py
@@ -28,6 +28,7 @@ def arg_parse():
     parser.add_argument('--or_loss', action='store_true', help='Set or_loss to True if this flag is present')
 #     parser.add_argument('--path', type=str, default='/disk_194b/GCL_datasets/data') # 30
 #     parser.add_argument('--path', type=str, default='/disk_195a/qiannnhui/data') # 195
+#     parser.add_argument('--path', type=str, default='/disk_248a/qiannnhui/data') # 248
     parser.add_argument('--path', type=str, default='/home/qiannnhui/data/data') # 249
     parser.add_argument('--aug_ratio', dest='aug_ratio', type=int, default=1,
             help='Dropout rate of data augmentation, will be multiplied by 0.1')
